16f02f1bdfaacda2bf1d2006afa69359973ec9b3
Inderjit Singh <Inderjit.Singh@jet2.com>
2025-03-29 11:50:47 +0530

    adding code coverage flag in package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ButtonModule],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'online-bookstore';
-
-  
-}
+export class AppComponent {}
